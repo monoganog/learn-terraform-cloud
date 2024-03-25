a7099b96b958cbcf27e697a8c37f577a0c5959c1
Ben Monaghan <monoganog@gmail.com>
2024-03-25 23:49:46 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  instanceInitiatedShutdownBehavior = "stop" # or "terminate"
+  instanceInitiatedShutdownBehavior = stop
   tags = {
     Name = var.instance_name
   }
